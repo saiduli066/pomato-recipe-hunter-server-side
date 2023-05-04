f8c648d919d48f20e75b74c02fe69cd75c381633
Md Saidul Islam <saiduli066@gmail.comm>
2023-05-04 21:07:15 +0600

    versel.json file is added and deployed to vercel

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
-
+const chefs = require('./data.json')
 app.get('/', (req,res) => {
     res.send('Recipe hunter is running')
+})
+
+app.get('/chefs', (req, res) => {
+    res.send(chefs);
 })
 
 app.listen(port, () => {
