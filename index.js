3835f3fee247e0a4499f945579e7b1d45c550b9b
Md Saidul Islam <saiduli066@gmail.comm>
2023-05-04 22:10:12 +0600

    cors is installed and called

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const app = express();
+const cors = require("cors");
+
 const port = process.env.PORT || 5000;
+
 const chefs = require('./data.json')
+
+app.use(cors());
+
 app.get('/', (req,res) => {
     res.send('Recipe hunter is running')
 })
